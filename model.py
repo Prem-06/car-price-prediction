ca2274f10104ba67f7c35eb2fc57759e73b52cb8
Prem Prakash Pal <0610prem@gmail.com>
2024-08-15 18:17:42 +0530

    done

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 from sklearn.linear_model import LinearRegression
 import joblib
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
