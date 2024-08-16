8b885b9c67d7f428efd8d285eb75e72ce95f0fa9
Prem Prakash Pal <0610prem@gmail.com>
2024-08-16 11:55:33 +0530

    done

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+from waitress import serve
 
 model = joblib.load('model.pkl')
+
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-       
         data = request.json
-        price = data['price']
-        price = np.array([[price]])
+        if 'price' not in data:
+            return jsonify({'error': 'Price value is missing'}), 400
+        
+        price = np.array([[data['price']]])
         prediction = model.predict(price)
         return jsonify({'prediction': prediction[0]})
-    
+
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    serve(app, host='0.0.0.0', port=8000)
